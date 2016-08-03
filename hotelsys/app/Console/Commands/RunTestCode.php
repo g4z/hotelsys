42212d5114e4f79cf29dd8b2bcc773cf6cfb4880
Gareth Williams <panino@tuta.io>
2016-08-03 15:04:52 +0200

    added console command to run test code

---
+++ hotelsys/app/Console/Commands/RunTestCode.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RunTestCode extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hotelsys:run-test-code';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run some testing code';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // $hotel = \App\Hotel::find(1);
+        // dd($hotel->rooms->toArray());   
+    }
+}
